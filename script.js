4b250c70bd2b80f295218244d41ada68d0863dfb
TekniqueJoseph <joejacksun@gmail.com>
2023-03-29 19:35:22 -0400

    added more arrays

--- script.js
+++ script.js
@@ -1,46 +1,46 @@
-// const wu = ['rza', 'gza', 'odb', 'dec', 'ugod', 'meth', 'ghost', 'master killer', 'rae'];
+const wu = ['rza', 'gza', 'odb', 'dec', 'ugod', 'meth', 'ghost', 'master killer', 'rae'];
 
-// const members = wu.map (cru =>{
-//     console.log(cru)
-// })
+const members = wu.map (cru =>{
+    console.log(cru)
+})
 
-// const nameLength = wu.map(sel => {
-//     return sel.length;
-// })
-// console.log(nameLength)
+const nameLength = wu.map(sel => {
+    return sel.length;
+})
+console.log(nameLength)
 
 
-// const memberId = wu.map ((cru,index) =>{
-//     console.log(cru,index)
-// })
+const memberId = wu.map ((cru,index) =>{
+    console.log(cru,index)
+})
 
-// const wiz = [1,2,3,4,5,6,7,8,9];
+const wiz = [1,2,3,4,5,6,7,8,9];
 
-// const math = wiz.map(num => {
-//     console.log(num * 2)
-//  })
+const math = wiz.map(num => {
+    console.log(num * 2)
+ })
 
-// let total = 1;
-// const add = wiz.map((val) =>{
-//     total = total * val
-//   return val
-// })
-// console.log(total)
+let total = 1;
+const add = wiz.map((val) =>{
+    total = total * val
+  return val
+})
+console.log(total)
 
-// const scores = [[1, 2], [3, 4], [5, 6]];
+const scores = [[1, 2], [3, 4], [5, 6]];
 
-// const array1 = scores.map((subArrays) => {
-//     return subArrays;
-// }) 
-// console.log(array1)
+const array1 = scores.map((subArrays) => {
+    return subArrays;
+}) 
+console.log(array1)
 
-// const array2 = scores.map((nestedArrays) => {
-//     return nestedArrays.map((nums) => {
-//         return nums * 2
-//     })
-// })
+const array2 = scores.map((nestedArrays) => {
+    return nestedArrays.map((nums) => {
+        return nums * 2
+    })
+})
 
-// console.log(array2)+
+console.log(array2)
 
 const arr1 = [1,2,3];
 const arr2 = [4,5,6];
