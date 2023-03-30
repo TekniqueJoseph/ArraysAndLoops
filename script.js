36db4ea8604a8da9e3317cdf194aec168e653240
TekniqueJoseph <joejacksun@gmail.com>
2023-03-30 18:49:28 -0400

    added array methods

--- script.js
+++ script.js
@@ -34,72 +34,86 @@ const add = wiz.map((val) =>{
   return total
 })
 console.log(`The value returned is: ${total}`)
-
+// 
 const scores = [[1, 2], [3, 4], [5, 6]];
-
+// map method iterates through array scores and returns an array of the sub arrays
 const array1 = scores.map((subArrays) => {
     return subArrays;
 }) 
 console.log(array1)
 
-// const array2 = scores.map((nestedArrays) => {
-//     return nestedArrays.map((nums) => {
-//         return nums * 2
-//     })
-// })
+// the code below makes use of the map method twice. the first use iterates through array2 and returns and stores the sub arrays
+// the map metod iterates through the sub arrays and each value is multiplied by 2
+const array2 = scores.map((nestedArrays) => {
+    return nestedArrays.map((nums) => {
+        return nums *2
+    })
+})
 
-// console.log(array2)
+console.log('nested',array2)
+// the cose below shows hot the concat method can beused to combine two arrays
+const arr1 = [1,2,3];
+const arr2 = [4,5,6];
+console.log(arr1); // [1,2,3]
+console.log(arr2); // [4,5,6]
+// Merge arrays
+const merged = arr1.concat(arr2);
+console.log('merged',merged); // [1,2,3,4,5,6]
+// the code below maps through the combined arrays and mu
+const combined = (arr1.concat(arr2).map(num => {
+    return `Value times 5 = ${num * 5}`
+}))
+console.log('combined',combined)
 
-// const arr1 = [1,2,3];
-// const arr2 = [4,5,6];
+// string array
+let firstNames = ["super", "spider", "ant", "iron"]
+let lastName = "man";
+// the code below maps through array firstNames and returns the values with variable lastNae concated to it
+const fullname = firstNames.map((full) =>{
+    return full+lastName;
+})
+console.log(fullname)
+// numbers array
+let numbers = [3.7, 4.9, 6.2]
+// the code below maps throgh the numbers array and uses the map method to round the numbers
+let rounded = numbers.map((nums) => {
+    return Math.round(nums)
+})
+console.log(rounded)
+// super heros array
+let frontNane = ["wonder", "super", "spider", "ant", "iron", "method"]
+let male = "man"
+let female = "woman"
+// the code below maps through the frontName array and adds the appropriate gender variable to the front name value
+// The ternary operator ? is basically a shortcut for a traditional if...else statement, with the condition and value if true or false
+let wholeName = frontNane.map((frontNane, index) => {
+    return (index == 0) ?  frontNane + female : frontNane + male;
+})
+console.log(wholeName)
+// the code below maps through the wholename array created above, and with trmplate strings, returns the value and it's index  
+const newName = wholeName.map((theName, index) =>{
+    return `${theName} is at index # ${index}`
+})
 
-// // Merge arrays
-// // const merged = arr1.concat(arr2);
+console.log(newName)
 
-// // console.log(merged); // [1,2,3,4,5,6]
-// console.log(arr1); // [1,2,3]
-// console.log(arr2); // [4,5,6]
+// ******filter method - The filter() method creates a shallow copy of a portion of a given array, filtered down to just the elements from the given array that pass the test implemented by the provided function.******
+const wuTang = ['rza', 'gza', 'odb', 'dec', 'ugod', 'meth', 'ghost', 'master killer', 'rae', 'cappadonna'];
 
-// const combined = (arr1.concat(arr2).map(num => {
-//     return num * 5 + ' Hello'
-// }))
+const results = wuTang.filter((chars) => {
+    return chars.length > 3
+})
+console.log(results)
 
-// console.log('combined',combined)
+const ages = [32, 33, 16, 40];
 
-// let firstNames = ["super", "spider", "ant", "iron"]
-// let lastName = "man";
+const ageCheck = ages.filter((age) => {
+      return age >= 18
+})
 
-// const fullname = firstNames.map((full) =>{
-//     return full+lastName;
-// })
+console.log(ageCheck)
 
-// console.log(fullname)
 
-// let numbers = [3.7, 4.9, 6.2]
-
-// let rounded = numbers.map((nums) => {
-//     return Math.round(nums)
-// })
-// console.log(rounded)
-
-// let frontNane = ["wonder", "super", "spider", "ant", "iron", "method"]
-// let male = "man"
-// let female = "woman"
-
-// let wholeName = frontNane.map((frontNane, index) => {
-//     return (index == 0) ?  frontNane + female : frontNane + male;
-// })
-
-// console.log(wholeName)
-   
-// const newName = wholeName.map((theName, index) =>{
-//     return `${theName} is at index # ${index}`
-// })
-
-// console.log(newName)
-
-// **********map***********
-// ******Filter Array******
 // ****Reduce Array********
 // reduce method iterates through reduceNums array and multiplies each array value(curr), by accu whos's initial value is 1. The reduce method does not return an array. It simply returns a value
 
@@ -133,3 +147,6 @@ console.log(array1)
 // extends
 // clases
 // class ChildClass extends ParentClass { /* … */ }
+// super
+
+// fetch
