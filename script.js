723b030e09ec622611df6a046585fdb49e5451dc
TekniqueJoseph <joejacksun@gmail.com>
2023-03-29 21:56:07 -0400

    added more types of arrays

--- script.js
+++ script.js
@@ -1,91 +1,104 @@
+// String Array
 const wu = ['rza', 'gza', 'odb', 'dec', 'ugod', 'meth', 'ghost', 'master killer', 'rae'];
-
+// map method iterates through the wu array and returns each string from the array wu into new array members
 const members = wu.map (cru =>{
     console.log(cru)
 })
 
-const nameLength = wu.map(sel => {
-    return sel.length;
+// map mathod iterates through the wu array and returns the length of each string into array nameLength 
+const nameLength = wu.map(name => {
+    return name.length;
 })
 console.log(nameLength)
 
-
+// map method iterates through the wu array and returns each sting with the index of the coresonding string into array mamaber Id
 const memberId = wu.map ((cru,index) =>{
     console.log(cru,index)
 })
 
+// map method iterates through wiz array multiplies each number ny 2. the reults are stored in array math
 const wiz = [1,2,3,4,5,6,7,8,9];
 
 const math = wiz.map(num => {
     console.log(num * 2)
  })
 
+//  map method iterates through wiz array and multiplies each value by let total which is initialzed with a value of 1
 let total = 1;
 const add = wiz.map((val) =>{
     total = total * val
   return val
 })
-console.log(total)
+console.log('regula', total)
 
-const scores = [[1, 2], [3, 4], [5, 6]];
+// reduce method iterates through wiz array and multiplies each array value(curr), by accu whs's initial value is 1
+let reduce = wiz.reduce((accu,curr) => {
+    return accu * curr
+},1)
+console.log('reduce',reduce)
 
-const array1 = scores.map((subArrays) => {
-    return subArrays;
-}) 
-console.log(array1)
+// const scores = [[1, 2], [3, 4], [5, 6]];
 
-const array2 = scores.map((nestedArrays) => {
-    return nestedArrays.map((nums) => {
-        return nums * 2
-    })
-})
+// const array1 = scores.map((subArrays) => {
+//     return subArrays;
+// }) 
+// console.log(array1)
 
-console.log(array2)
+// const array2 = scores.map((nestedArrays) => {
+//     return nestedArrays.map((nums) => {
+//         return nums * 2
+//     })
+// })
 
-const arr1 = [1,2,3];
-const arr2 = [4,5,6];
+// console.log(array2)
 
-// Merge arrays
-// const merged = arr1.concat(arr2);
+// const arr1 = [1,2,3];
+// const arr2 = [4,5,6];
 
-// console.log(merged); // [1,2,3,4,5,6]
-console.log(arr1); // [1,2,3]
-console.log(arr2); // [4,5,6]
+// // Merge arrays
+// // const merged = arr1.concat(arr2);
 
-const combined = (arr1.concat(arr2).map(num => {
-    return num * 5 + ' Hello'
-}))
+// // console.log(merged); // [1,2,3,4,5,6]
+// console.log(arr1); // [1,2,3]
+// console.log(arr2); // [4,5,6]
 
-console.log('combined',combined)
+// const combined = (arr1.concat(arr2).map(num => {
+//     return num * 5 + ' Hello'
+// }))
 
-let firstNames = ["super", "spider", "ant", "iron"]
-let lastName = "man";
+// console.log('combined',combined)
 
-const fullname = firstNames.map((full) =>{
-    return full+lastName;
-})
+// let firstNames = ["super", "spider", "ant", "iron"]
+// let lastName = "man";
 
-console.log(fullname)
+// const fullname = firstNames.map((full) =>{
+//     return full+lastName;
+// })
 
-let numbers = [3.7, 4.9, 6.2]
+// console.log(fullname)
 
-let rounded = numbers.map((nums) => {
-    return Math.round(nums)
-})
-console.log(rounded)
+// let numbers = [3.7, 4.9, 6.2]
 
-let frontNane = ["wonder", "super", "spider", "ant", "iron"]
-let male = "man"
-let female = "woman"
+// let rounded = numbers.map((nums) => {
+//     return Math.round(nums)
+// })
+// console.log(rounded)
 
-let wholeName = frontNane.map((frontNane, index) => {
-    return (index == 0) ?  frontNane + female : frontNane + male;
-})
+// let frontNane = ["wonder", "super", "spider", "ant", "iron", "method"]
+// let male = "man"
+// let female = "woman"
 
-console.log(wholeName)
+// let wholeName = frontNane.map((frontNane, index) => {
+//     return (index == 0) ?  frontNane + female : frontNane + male;
+// })
+
+// console.log(wholeName)
    
-const newName = wholeName.map((theName, index) =>{
-    return `${theName} is at index # ${index}`
-})
+// const newName = wholeName.map((theName, index) =>{
+//     return `${theName} is at index # ${index}`
+// })
 
-console.log(newName)
+// console.log(newName)
+
+// ******Filter Array******
+// *******Reduce Array*****
